b0b0de43b43848008c61516fa81f63a40182442b
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-09-05 11:46:22 -0300

    feat: create user registration

--- src/main/java/com/picpaydesafiobackend/services/UserService.java
+++ src/main/java/com/picpaydesafiobackend/services/UserService.java
@@ -2,11 +2,13 @@ package com.picpaydesafiobackend.services;
 
 import com.picpaydesafiobackend.domain.user.User;
 import com.picpaydesafiobackend.domain.user.UserType;
+import com.picpaydesafiobackend.dtos.UserDTO;
 import com.picpaydesafiobackend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -25,6 +27,16 @@ public class UserService {
 
     public User findUserById(Long id) throws Exception {
         return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public User createUser(UserDTO data) {
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers() {
+       return this.repository.findAll();
     }
 
     public void saveUser(User user) {
